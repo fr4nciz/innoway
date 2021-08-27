fe6858cc986bf674c067695f5f245211e69e03e9
fr4nciz <exl_francis@hotmail.com>
2021-08-27 10:33:52 -0300

    index page

---
+++ assets/js/utils.js
@@ -0,0 +1,51 @@
+// $(document).on("scroll", function () {
+// 	if ($(document).scrollTop() > 100) {
+// 		$("header").addClass("small");
+	
+// 	} else {
+// 		$("header").removeClass("small");
+// 	}
+// });
+
+$(document).ready(function () {
+	$('#sidebarCollapse').on('click', function () {
+		$('#sidebar').toggleClass('active');
+		$(this).toggleClass('active');
+	});
+
+    $('.fechar-menu').on('click', function () {
+		$('#sidebar').toggleClass('active');	
+        $('#sidebarCollapse').toggleClass('active');
+	});
+
+    $('.bt-data').click(function() {
+        $(this).fadeOut();
+        $('.header-content ').toggleClass('act')
+      });
+
+      $('.fechar-h').click(function() {   
+        $('.bt-data').fadeIn();   
+        $('.header-content ').removeClass('act')
+      });
+
+
+      
+
+   
+});
+
+x = 0
+function changeOwner() {
+    if (!x) {
+        x=1
+        document.querySelector('body').classList.remove('partner-report')
+        document.querySelector('body').classList.add('own-report')
+    } else {
+        x=0
+        document.querySelector('body').classList.remove('own-report')
+        document.querySelector('body').classList.add('partner-report')
+
+    }
+
+}
+
